18c3a88f92a9d727b2da801a4b267ddc0253c036
Eidmone <eidmonetagaca@gmail.com>
2021-08-21 15:41:16 -0700

    fixed images

--- .history/src/components/Timeline_20210820215205.js
+++ .history/src/components/Timeline_20210820215205.js
@@ -98,6 +98,37 @@ const Timeline = () => {
           </ul>
         </p1>
       </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        // date="2008 - 2010"
+        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        // icon={<WorkIcon />}
+      >
+        <h2 className="vertical-timeline-element-title">
+          Video Specialist - Cal State University, Bakersfield
+        </h2>
+        <h2 className="vertical-timeline-element-subtitle">
+          Nov 2020 - Current
+        </h2>
+        <p1>
+          <ul className="description">
+            <li>
+              Made over 50+ informative e-learning videos in a variety of
+              subjects that enabled students to have a positive and immersive
+              learning experience.
+            </li>
+            <li>
+              Held an event with 40+ attendees through a Zoom meeting that
+              catered to students' needs in navigating the school's website and
+              portal.
+            </li>
+            <li>
+              Managed and created engaging social media content that increased
+              student engagement by 25%.
+            </li>
+          </ul>
+        </p1>
+      </VerticalTimelineElement>
     </VerticalTimeline>
   );
 };
